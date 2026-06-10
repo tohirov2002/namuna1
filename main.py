11be62a9745c93b16ac9e7d1012e818ab29ee6a6
tohirov2002 <tohirovahmad1519@gmail.com>
2026-06-10 11:01:58 +0500

    yangiladim

---
+++ main.py
@@ -0,0 +1,26 @@
+#summa = 600
+
+#if summa > 300 and summa < 500:
+#    print("Tabriklayman siz yashil makonga utdingiz")
+#elif summa > 500:
+#    print("tabriklayman siz tog'li zonaga utdingiz")
+#else:
+#    print("Sizda mablag' yetarli emas")
+
+
+# yosh = 19
+
+# if yosh > 18:
+#     print("siz sayohatga bora olasiz")
+# else:
+#     print("Tabriklayman siz hali sayohatga bora olmaysiz siz borish uchun uqishingiz kerak")
+    
+
+level = 45
+
+if level == 12: # x
+    print("Tabriklayaman siz Arina zonaga utdingiz")
+elif level == 45: # x
+    print("tabriklayman siz city zonaga utdingiz")
+else:
+    print("Hali siz boshlang'ich leveldasiz o'yinni uchirib uqing")
